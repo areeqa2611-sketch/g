3532c3881fc4d1b9b6c920fbd60d57b90e4980b3
areeqa2611-sketch <areeqa2611@gmail.com>
2026-09-02 16:54:09 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='omoro-lab-v081';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
